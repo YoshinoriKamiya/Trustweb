4d6966039c77af2c08e1ff312e34e231584fb902
YoshinoriKamiya <78638073+YoshinoriKamiya@users.noreply.github.com>
2023-10-12 00:10:57 +0900

    Update main.js
    スライダー修正

--- main.js
+++ main.js
@@ -73,13 +73,13 @@ const swiper = new Swiper('#swiper-mainvisual', {
       arrows: false,
       dotsClass: 'slider-dots',
       variableWidth:true,
-      
+      centerPadding: '20px',//ポイント！！！
       responsive:[
         {
           breakpoint:968,
           settings:{
           slidesToShow:1,
-          variableWidth:false,
+          variableWidth:true,
         }},
         {
           breakpoint: 320,
@@ -106,13 +106,27 @@ $(document).ready(function(){
       dots: true,
       arrows: false,
       dotsClass: 'slider-dots',
+      variableWidth:true,
       responsive:[
         {
           breakpoint:968,
           settings:{
           slidesToShow:1,
-          // variableWidth:true,
+          variableWidth:true,
         }},
+        {
+          breakpoint:525,
+          settings:{
+          slidesToShow:1,
+          variableWidth:false,
+        }},
+        {
+          breakpoint: 320,
+          settings: {
+            slidesToShow: 1,
+            variableWidth:false,
+          }
+        },
       ],
     });
   });
@@ -121,53 +135,72 @@ $(document).ready(function(){
 円を描画するアニメーション  半径:200px
 -------------------------------------------*/
   
-      // スクロールイベント
-      window.addEventListener('scroll', function() {
-        // アニメーションを呼び出す
-        animateCircle();
-    });
+  //    // スクロールイベント
+  //    window.addEventListener('scroll', function() {
+  //     // アニメーションを呼び出す
+  //     animateCircle();
+  // });
 
-    function animateCircle() {
-        // 要素の縦位置を取得
-        let target = document.querySelector('.svg1').getBoundingClientRect().top;
-        // スクロール量を取得
-        let scroll = window.scrollY;
-        // 表示画面の高さを取得
-        let windowHeight = window.innerHeight;
+  // function animateCircle() {
+  //     // 要素の縦位置を取得
+  //     let target = document.querySelector('.svg1').getBoundingClientRect().top;
+  //     // スクロール量を取得
+  //     let scroll = window.scrollY;
+  //     // 表示画面の高さを取得
+  //     let windowHeight = window.innerHeight;
 
-        // アニメーションをトリガーする条件
-        if (scroll > target - windowHeight) {
-            document.querySelector('.circle1').style.animation = 'circle1 2s forwards'; // アニメーションを有効に
-        }
-    }
-    // 初回のスクロールイベントをトリガー
-    animateCircle();
+  //     // アニメーションをトリガーする条件
+  //     if (scroll > target - windowHeight + 0) {
+  //         document.querySelector('.circle1').style.animation = 'circle1 2s forwards'; // アニメーションを有効に
+  //     }
+  // }
+  // // 初回のスクロールイベントをトリガー
+  // animateCircle();
+  //    // スクロールイベント
+  //    window.addEventListener('scroll', function() {
+  //     // アニメーションを呼び出す
+  //     animateCircle();
+  // });
 
+  // function animateCircle() {
+  //     // 要素の縦位置を取得
+  //     let target = document.querySelector('.svg2').getBoundingClientRect().top;
+  //     // スクロール量を取得
+  //     let scroll = window.scrollY;
+  //     // 表示画面の高さを取得
+  //     let windowHeight = window.innerHeight;
 
+  //     // アニメーションをトリガーする条件
+  //     if (scroll > target - windowHeight + 0) {
+  //         document.querySelector('.circle2').style.animation = 'circle2 2s forwards'; // アニメーションを有効に
+  //     }
+  // }
+  // // 初回のスクロールイベントをトリガー
+  // animateCircle();
   /*-------------------------------------------
 円を描画するアニメーション  半径:160px
 -------------------------------------------*/
-    // スクロールイベント
-    window.addEventListener('scroll', function() {
-      // アニメーションを呼び出す
-      animateCircle();
-  });
+//  // スクロールイベント
+//  window.addEventListener('scroll', function() {
+//   // アニメーションを呼び出す
+//   animateCircle();
+// });
 
-  function animateCircle() {
-      // 要素の縦位置を取得
-      let target = document.querySelector('.svg2').getBoundingClientRect().top;
-      // スクロール量を取得
-      let scroll = window.scrollY;
-      // 表示画面の高さを取得
-      let windowHeight = window.innerHeight;
+// function animateCircle() {
+//   // 要素の縦位置を取得
+//   let target = document.querySelector('.svg2,.sv').getBoundingClientRect().top;
+//   // スクロール量を取得
+//   let scroll = window.scrollY;
+//   // 表示画面の高さを取得
+//   let windowHeight = window.innerHeight;
 
-      // アニメーションをトリガーする条件
-      if (scroll > target - windowHeight ) {
-          document.querySelector('.circle2').style.animation = 'circle2 2s forwards'; // アニメーションを有効に
-      }
-  }
-  // 初回のスクロールイベントをトリガー
-  animateCircle();
+//   // アニメーションをトリガーする条件
+//   if (scroll > target - windowHeight + 200) {
+//       document.querySelector('.circle2').style.animation = 'circle2 2s forwards'; // アニメーションを有効に
+//   }
+// }
+// // 初回のスクロールイベントをトリガー
+// animateCircle();
   /*-------------------------------------------
 画面を下にスクロールしていくと
 アルファベットがランダムに変化してWe Value Serendipity.が出現
